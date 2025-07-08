2ca3992c4685ae82e14489180be4725310cd6676
OMOISIOMOBE <ocharo34@gmail.com>
2025-07-08 14:45:15 +0300

    county drop down on employee_register and employer

--- employer_register.php
+++ employer_register.php
@@ -53,7 +53,57 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
     <form method="POST">
         <input type="text" name="name" placeholder="Full Name" required>
-        <input type="text" name="location" placeholder="Location" required>
+        <select name="location" required>
+          <option value="" disabled selected>Select your county</option>
+          <option value="baringo">Baringo</option>
+          <option value="bomet">Bomet</option>
+          <option value="bungoma">Bungoma</option>
+          <option value="busia">Busia</option>
+          <option value="elgeyo-marakwet">Elgeyo-Marakwet</option>
+          <option value="embu">Embu</option>
+          <option value="garissa">Garissa</option>
+          <option value="homa-bay">Homa Bay</option>
+          <option value="isiolo">Isiolo</option>
+          <option value="kajiado">Kajiado</option>
+          <option value="kakamega">Kakamega</option>
+          <option value="kericho">Kericho</option>
+          <option value="kiambu">Kiambu</option>
+          <option value="kilifi">Kilifi</option>
+          <option value="kirinyaga">Kirinyaga</option>
+          <option value="kisii">Kisii</option>
+          <option value="kisumu">Kisumu</option>
+          <option value="kitui">Kitui</option>
+          <option value="kwale">Kwale</option>
+          <option value="laikipia">Laikipia</option>
+          <option value="lamu">Lamu</option>
+          <option value="machakos">Machakos</option>
+          <option value="makueni">Makueni</option>
+          <option value="mandera">Mandera</option>
+          <option value="meru">Meru</option>
+          <option value="migori">Migori</option>
+          <option value="marsabit">Marsabit</option>
+          <option value="mombasa">Mombasa</option>
+          <option value="murang'a">Murang'a</option>
+          <option value="nairobi">Nairobi</option>
+          <option value="nakuru">Nakuru</option>
+          <option value="nandi">Nandi</option>
+          <option value="narok">Narok</option>
+          <option value="nyamira">Nyamira</option>
+          <option value="nyandarua">Nyandarua</option>
+          <option value="nyeri">Nyeri</option>
+          <option value="samburu">Samburu</option>
+          <option value="siaya">Siaya</option>
+          <option value="taita-taveta">Taita-Taveta</option>
+          <option value="tana-river">Tana River</option>
+          <option value="tharaka-nithi">Tharaka-Nithi</option>
+          <option value="trans-nzoia">Trans Nzoia</option>
+          <option value="turkana">Turkana</option>
+          <option value="uasin-gishu">Uasin Gishu</option>
+          <option value="vihiga">Vihiga</option>
+          <option value="wajir">Wajir</option>
+          <option value="west-pokot">West Pokot</option>
+        </select>
+
         <select name="residence" required>
             <option value="">Residence Type</option>
             <option value="urban">Urban</option>
